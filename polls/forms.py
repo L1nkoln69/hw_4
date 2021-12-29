0d448a01d7dd08ed30954c0f488ee20e4f46d252
[L1nkoln] <orlov229003@gmail.com>
2021-12-29 20:23:43 +0200

    new hw_7

--- polls/forms.py
+++ polls/forms.py
@@ -2,6 +2,19 @@ from django import forms
 
 
 class Triangle(forms.Form):
-    Cathetus_1 = forms.FloatField(label='Катет первый')
-    Cathetus_2 = forms.FloatField(label='Катет второй')
-    Hypotenuse = None
+    Cathetus_1 = forms.DecimalField(label='Катет 1')
+    Cathetus_2 = forms.DecimalField(label='Катет 2')
+
+    def new_athetus_1(self):
+        Cathetus_1 = self.cleaned_data.get('Cathetus_1')
+        if Cathetus_1 < 0:
+            raise forms.ValidationError('неправильоне значение')
+        else:
+            return Cathetus_1
+
+    def new_athetus_2(self):
+        Cathetus_2 = self.cleaned_data.get('Cathetus_1')
+        if Cathetus_2 < 0:
+            raise forms.ValidationError('неправильоне значение')
+        else:
+            return Cathetus_2
