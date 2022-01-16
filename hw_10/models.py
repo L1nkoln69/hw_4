56cbd12fb62d1ac57306314627c7eb376715b93b
[L1nkoln] <orlov229003@gmail.com>
2022-01-16 23:01:01 +0200

    доделано дз 10

--- hw_10/models.py
+++ hw_10/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class City(models.Model):
     name = models.CharField(max_length=30)
     suppliers = models.OneToOneField('Supplier', on_delete=models.CASCADE)
-    clients = models.ForeignKey('Client', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.name}'
@@ -12,6 +11,7 @@ class City(models.Model):
 
 class Client(models.Model):
     first_name = models.CharField(max_length=40)
+    city = models.ForeignKey('City', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.first_name}'
